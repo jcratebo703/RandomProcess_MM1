07db35a761e4318175ed3eeac9646c97e0c27fc8
jcratebo703 <jcratebo703@gmail.com>
2020-01-11 01:42:02 +0800

    60%

---
+++ MM1_simulation.py
@@ -0,0 +1,126 @@
+import random
+import numpy as np
+
+next_event = "arrival"
+next_event_time = 0
+next_arrival = 0
+now = 0
+end_time = 1000
+stack_time = {}
+stack_event = {}
+top = len(stack_event) - 1
+lamb = 1
+previous_event_time = 0
+system_size = 0
+queue_size = 0
+
+
+def expRVGenerator(para):
+    a = 0
+    b = 1
+    U = random.uniform(a, b)
+    x = -np.log(1 - U) / para
+    result = a + (b - a) * x
+
+    return result
+
+
+def event_ID():
+    if next_event == "arrival":
+        return 0
+    elif next_event == "service":
+        return 1
+    elif next_event == "departure":
+        return 2
+
+
+def stack_push(event, time):
+    global top
+    j = 0
+    top = len(stack_event) - 1
+
+    for x in range(len(stack_time)):
+        if stack_time[x] > time:
+            j = x
+            break
+
+    for i in range(top + 1, -1, -1):
+        if i <= j:
+            break
+
+        stack_time[i] = stack_time[i - 1]
+        stack_event[i] = stack_event[i - 1]
+
+    stack_time[j] = time
+    stack_event[j] = event
+
+
+def stack_pop():
+    global top, next_event, next_event_time
+    next_event = stack_event[0]
+    next_event_time = stack_time[0]
+    top = len(stack_event) - 1
+
+    for i in range(0, top - 1):
+        stack_time[i] = stack_time[i + 1]
+        stack_event[i] = stack_event[i + 1]
+
+    stack_event.pop(top)
+    stack_time.pop(top)
+
+
+def arrival():
+    global now, next_event, previous_event_time, next_arrival, queue_size
+    now = next_event_time
+    next_arrival = now + expRVGenerator(lamb)
+    stack_push("arrival", next_arrival)
+    if system_size == 0:
+        next_event = "service"
+    else:
+        stack_pop()
+
+    queue_size += 1
+    statistics()
+    previous_event_time = now
+
+
+def service():
+    global now, next_arrival, previous_event_time
+    now = next_event_time
+    next_arrival = now + expRVGenerator(lamb)
+    stack_push("departure", next_arrival)
+    stack_pop()
+
+    statistics()
+    previous_event_time = now
+
+
+def departure():
+    global now, next_event, previous_event_time
+    now = next_event_time
+
+    if queue_size > 0:
+        next_event = "service"
+    else:
+        stack_pop()
+
+    statistics()
+    previous_event_time = now
+
+
+def statistics():
+    pass
+
+
+while now < end_time:
+    print(stack_time)
+    print(stack_event)
+    if event_ID() == 0:
+        arrival()
+        continue
+    elif event_ID() == 1:
+        service()
+        continue
+    elif event_ID() == 2:
+        departure()
+        continue
